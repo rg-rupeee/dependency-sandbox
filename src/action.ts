856c7879b7bcaccf1d5b769c47345e290759f8ba
rg-rupeee <rupeshgarhwal3920@gmail.com>
2024-12-26 19:32:46 +0530

    action strategy executer

---
+++ src/action.ts
@@ -0,0 +1,57 @@
+import inquirer from 'inquirer';
+import { REPLSandbox } from './modes/repl.js';
+import { createSandbox } from './modes/sandbox.js';
+import Logger from './utils/logger.js';
+
+export const MODES = {
+  REPL: 'repl',
+  PLAYGROUND: 'playground',
+};
+
+async function promptUser() {
+  return await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'mode',
+      message: 'Select the execution mode:',
+      choices: Object.values(MODES),
+      default: MODES.REPL,
+    },
+  ]);
+}
+
+interface IConfig {
+  mode: string;
+}
+
+async function executeStrategy(packages: string[], config: IConfig) {
+  if (config.mode === MODES.REPL) {
+    Logger.blue('Starting REPL mode...');
+    const sandbox = new REPLSandbox();
+    await sandbox.start(packages);
+  } else if (config.mode === MODES.PLAYGROUND) {
+    Logger.blue('Starting Playground mode...');
+    await createSandbox(packages);
+  } else {
+    Logger.error('Invalid mode specified.');
+    process.exit(1);
+  }
+}
+
+export const action = async (packages: string[], options: any) => {
+  const config: IConfig = {
+    mode: options.mode,
+  };
+
+  if (options.default) {
+    Logger.info('Using default values...');
+    config.mode = MODES.REPL;
+  }
+
+  if (!config.mode) {
+    const answers = await promptUser();
+    config.mode = answers.mode;
+  }
+
+  executeStrategy(packages, config);
+};
--- src/modes/sandbox.ts
+++ src/modes/sandbox.ts
@@ -4,7 +4,7 @@ import fs from 'fs-extra';
 import { execSync } from 'child_process';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import Logger from './logger.js';
+import Logger from '../utils/logger.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
