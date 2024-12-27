6d2a558ad4ceee55982f8719791457ed564de86b
rg-rupeee <rupeshgarhwal3920@gmail.com>
2024-12-27 12:51:49 +0530

    Fix: Factory cofig param

--- src/modes/Factory.ts
+++ src/modes/Factory.ts
@@ -4,15 +4,13 @@ import { IConfig, IMode } from './Base.js';
 import Playground from './Playground.js';
 import Repl from './Repl.js';
 
-
-
 export class ModeFactory {
   static createMode(config: IConfig): IMode {
     switch (config.mode) {
       case MODES.PLAYGROUND:
-        return new Playground({ path: config.path });
+        return new Playground(config);
       case MODES.REPL:
-        return new Repl({ path: config.path });
+        return new Repl(config);
       default:
         Logger.error('Invalid mode type');
         process.exit(1);
